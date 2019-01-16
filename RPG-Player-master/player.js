9bd443d36cc2670acd1ec104332d2eed15bb9ddc
Levistpatrick <42749570+Levistpatrick@users.noreply.github.com>
2019-01-16 14:02:40 -0600

    Add files via upload

---
+++ RPG-Player-master/player.js
@@ -0,0 +1,61 @@
+//generic stats object
+
+let p1Name = "";
+
+let p1Stats = {
+
+}
+
+let p2Name = "";
+
+let p2Stats = {
+
+}
+
+let battle = {
+
+  attack: function(){},
+  defend: function(){}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let p1Pack = [2,2,1,0,1,1,0,0];
+
+
+//Assembling your player.
+
+let player1 = {
+  name: p1Name,
+  stats: p1Stats,
+  moves: p1Moves,
+  use: function(){
+    //fill in with a ton of if and else ifs
+  },
+  pack: myPack,
+  battle: {
+    attack: function(mv){
+      //fill in logic
+    },
+    defend: function(atkmv){
+      //fill in logic
+    }
+  }
+}
